96d6b952d3de571b25cfa9f5b6bd4332a6f6cac9
Bappy6155 <105677054+Bappy6155@users.noreply.github.com>
2022-12-01 22:01:51 +0600

    PRACTICE DAY 26.5

---
+++ practice26.5/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum=0;
+    int* ptr;
+    int* ptr2;
+    scanf("%d %d",&a,&b);
+    ptr=&a;
+    ptr2=&b;
+    sum=*ptr+*ptr2;
+    printf("%.4f\n",sum/2.0);
+}
